2ddae52032d4844aeb039ac621e9ab32d615b882
marcos-2810 <marcos281092@gmail.com>
2026-04-14 15:53:12 +0200

    Remove build artifacts from tracking, add .gitignore

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_colors_game_ColorsApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.colors.game.ColorsApplication"
-)
-public final class _com_colors_game_ColorsApplication {
-}
